c08f9b0be4741842da6b467603ef749d117c7b3c
Jorge <jorgejapao00@gmail.com>
2024-10-16 12:18:27 -0300

    Now displays the album name over the album cover

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ def show_rp():
     global rp_running
     rp_running = True
     while rp_running:
-        musica_atual, artista_atual, album_art = get_music()  # Pega a música atual
+        musica_atual, artista_atual, album_art, album_name = get_music()  # Pega a música atual
         if musica_atual and artista_atual:
             # Atualiza o Rich Presence do Discord
             RPC.update(
                 details=f"{musica_atual}",
                 state=f"{artista_atual}",
                 large_image=album_art if album_art else "default_image",  # Use uma imagem padrão se não houver capa
-                large_text="Álbum"  # Você pode mudar isso se quiser incluir mais informações
+                large_text=f"{album_name}"  # Você pode mudar isso se quiser incluir mais informações
             )
             time.sleep(3)
         else:
