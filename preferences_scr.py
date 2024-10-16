3c2038f85173af5f83c7919b19d78ed3e7bce13e
Jorge <jorgejapao00@gmail.com>
2024-10-16 11:42:24 -0300

    Screen to edit preferences created

---
+++ preferences_scr.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import ttk
+from preferences import *
+
+
+root = tk.Tk()
+root.title("Preferences")
+
+# Frame para organizar os widgets
+frame = ttk.Frame(root, padding="10")
+frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+# Labels e Entradas (Entry)
+ttk.Label(frame, text="User:").grid(column=1, row=1, sticky=tk.W)
+e1 = ttk.Entry(frame, width=30)
+e1.grid(column=2, row=1)
+
+ttk.Label(frame, text="API Key:").grid(column=1, row=2, sticky=tk.W)
+e2 = ttk.Entry(frame, width=30)
+e2.grid(column=2, row=2)
+
+# Botão de Envio
+submit_button = ttk.Button(frame, text="Save", command=lambda:save_pref(user=e1.get(), key=e2.get()))
+submit_button.grid(column=2, row=3, pady=10)
+
+# Para garantir que as colunas ajustem ao tamanho do conteúdo
+for child in frame.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+root.mainloop()
