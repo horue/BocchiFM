0f3f2bdfffd1cf54fe0dd296aab013a3545e13f4
Jorge <jorgejapao00@gmail.com>
2024-10-17 13:16:34 -0300

    General d file remade, now takes info from preferences.bcc

---
+++ d.py
@@ -0,0 +1,13 @@
+import configparser
+from cid import *
+
+config = configparser.ConfigParser()
+config.read('pref.bcc')
+user = config.get('user', 'u')
+apikey = config.get('user', 'k')
+
+
+
+CLIENT_ID = c_id
+API_KEY = apikey
+USERNAME = user
