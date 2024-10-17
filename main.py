a8f7bc2020248d4cbb22b2c7bd321be89e3a9c47
Jorge <jorgejapao00@gmail.com>
2024-10-17 13:14:17 -0300

    General show_rp function updated. The program wont crash and stomp anymore

--- main.py
+++ main.py
@@ -11,27 +11,32 @@ RPC.connect()  # Conecta ao Discord
 
 def main():
     while True:
-        musica_atual, artista_atual, album_art = get_music()  # Pega a música atual
+        musica_atual, artista_atual, album_art = get_music()  # Gets music
         update_discord_rpc(CLIENT_ID, musica_atual, artista_atual, album_art)  # Atualiza o Rich Presence
         time.sleep(3)
 
 def show_rp():
     global rp_running
     rp_running = True
-    while rp_running:
-        musica_atual, artista_atual, album_art, album_name = get_music()  # Pega a música atual
-        if musica_atual and artista_atual:
-            # Atualiza o Rich Presence do Discord
-            RPC.update(
-                details=f"{musica_atual}",
-                state=f"{artista_atual}",
-                large_image=album_art if album_art else "default_image",  # Use uma imagem padrão se não houver capa
-                large_text=f"{album_name}"  # Você pode mudar isso se quiser incluir mais informações
-            )
-            time.sleep(3)
-        else:
-            # Limpa o Rich Presence quando a música para de tocar
-            RPC.clear()
+    try:
+        while rp_running:
+            musica_atual, artista_atual, album_art, album_name = get_music()  # Gets music
+            if musica_atual and artista_atual:
+                # Atualiza o Rich Presence do Discord
+                RPC.update(
+                    details=f"{musica_atual}",
+                    state=f"{artista_atual}",
+                    large_image=album_art if album_art else "default_image",  # Use a default image if no cover is provided
+                    large_text=f"{album_name}",
+                )
+                time.sleep(3)
+            else:
+                RPC.clear()
+                time.sleep(10)
+                show_rp()
+    except Exception as e:
+        print(e)
+        time.sleep(2)
 
 def stop_rp():
     global rp_running
