370d8d492a14ba65c0babcf1b0c6f005e0d687c8
Jorge <jorgejapao00@gmail.com>
2024-10-17 13:19:16 -0300

    Tray menu updated. Preferences edition now works

--- tray.py
+++ tray.py
@@ -3,19 +3,12 @@ from main import *
 import pystray
 from PIL import Image, ImageDraw
 import threading
+from preferences_scr import *
 
 
 def create_image(width, height, color1, color2):
     # Generate an image and draw a pattern
-    image = Image.new('RGB', (width, height), color1)
-    dc = ImageDraw.Draw(image)
-    dc.rectangle(
-        (width // 2, 0, width, height // 2),
-        fill=color2)
-    dc.rectangle(
-        (0, height // 2, width // 2, height),
-        fill=color2)
-
+    image = Image.open('bcc_2.png')
     return image
 
 
@@ -31,21 +24,17 @@ def on_clicked(icon, item):
         threading.Thread(target=stop_rp, daemon=True).start()
 
     elif str(item) == "Preferences":
-        print("Not yet implemented.")
+        threading.Thread(target=edit_pref, daemon=True).start()
 
 
 menu_i = pystray.Menu(
-    pystray.MenuItem("Exit", on_clicked),
+    pystray.MenuItem(f"BocchiFM by horue.", None, enabled=False),
+    pystray.Menu.SEPARATOR,
     pystray.MenuItem("Start", on_clicked),
     pystray.MenuItem("Stop", on_clicked),
-    pystray.MenuItem("Preferences", on_clicked)
-
-
-
-    
-    
-    
-    
+    pystray.Menu.SEPARATOR,
+    pystray.MenuItem("Preferences", on_clicked),
+    pystray.MenuItem("Exit", on_clicked) 
 )
 
 
