9154f075579dab89fb2273b57d2477608128f3b7
Jorge <jorgejapao00@gmail.com>
2024-10-17 13:17:20 -0300

    get_music function updated with try-except and now gets album name

--- getfm.py
+++ getfm.py
@@ -16,32 +16,38 @@ url = f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={USER
 ultima_musica = None
 
 def get_music():
-    global ultima_musica
-    response = requests.get(url)
+    try:
+        global ultima_musica
+        response = requests.get(url)
 
-    # Verifica se a requisição foi bem-sucedida
-    if response.status_code != 200:
-        logging.error(f"Erro ao fazer requisição: {response.status_code}")
-        return None, None, None
+        # Verifica se a requisição foi bem-sucedida
+        if response.status_code != 200:
+            logging.error(f"Erro ao fazer requisição: {response.status_code}")
+            return None, None, None
 
-    data = response.json()
+        data = response.json()
 
-    if 'recenttracks' in data and 'track' in data['recenttracks']:
-        tracks = data['recenttracks']['track']
+        if 'recenttracks' in data and 'track' in data['recenttracks']:
+            tracks = data['recenttracks']['track']
 
-        for track in tracks:
-            if "@attr" in track and "nowplaying" in track["@attr"]:
-                musica_atual = track['name']
-                artista_atual = track['artist']['#text']
-                album_art = track['image'][2]['#text']  # Capa do álbum (tamanho médio)
-                album_name = track['album']['#text']
+            for track in tracks:
+                if "@attr" in track and "nowplaying" in track["@attr"]:
+                    musica_atual = track['name']
+                    artista_atual = track['artist']['#text']
+                    album_art = track['image'][2]['#text']  # Capa do álbum (tamanho médio)
+                    album_name = track['album']['#text']                
+                    if album_name == None:
+                        album_name = musica_atual
 
-                # Verifica se a música mudou
-                if musica_atual != ultima_musica:
-                    print(f"Agora tocando: {musica_atual} - {artista_atual}")
-                    ultima_musica = musica_atual  # Atualiza a última música
-                return musica_atual, artista_atual, album_art, album_name
-    return None, None, None, None  # Se não houver música tocando
+                    # Verifica se a música mudou
+                    if musica_atual != ultima_musica:
+                        print(f"Agora tocando: {musica_atual} - {artista_atual}")
+                        ultima_musica = musica_atual  # Atualiza a última música
+                    return musica_atual, artista_atual, album_art, album_name
+        return None, None, None, None  # Se não houver música tocando
+    except ValueError as e:
+        print(e)
+        get_music()
 
 
 def verify():
