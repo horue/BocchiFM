170c2d2499dc689542a350d95bee15885da64412
Jorge <jorgejapao00@gmail.com>
2024-10-17 13:28:02 -0300

    Now when the key or user is blank it keeps the current data

--- preferences.py
+++ preferences.py
@@ -5,15 +5,16 @@ from tkinter import messagebox
 
 config = configparser.ConfigParser()
 config.read('pref.bcc')
-
+user_r = config.get('user', 'u')
+apikey = config.get('user', 'k')
 
 def save_pref(user, key):
     if user == '':
-        pass
+        user = user_r
     else:
         config.set('user', 'u', str(user))
     if key == '':
-        pass
+        key = apikey
     else:
         config.set('user', 'k', str(key))
 
