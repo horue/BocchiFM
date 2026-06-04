24a156dd2b1a8dcb8ee3f4279c3fe458f09c6353
Jorge <jorgejapao00@gmail.com>
2026-06-04 15:22:49 -0300

    activity type changed to listening and track duration added

--- src/core/richpresence.py
+++ src/core/richpresence.py
@@ -2,6 +2,8 @@ import threading
 from pypresence import *
 from core.lastfm import *
 from core.config import CLIENT_ID
+from pypresence.types import ActivityType, StatusDisplayType
+import time
 
 
 RPC = Presence(CLIENT_ID) 
@@ -11,12 +13,14 @@ RPC.connect()
 class RichPresence():
     globalSound = ""
     globalArtist = ""
+    lastMusic = ""
     rp_running = False
+    agora = int(time.time())
+
 
 
     def updateMusicInfo():
         musicInfoString = f"Now playing {RichPresence.globalSound} by {RichPresence.globalArtist}."
-
 
 
     @staticmethod
@@ -26,17 +30,23 @@ class RichPresence():
         RichPresence.rp_running = True
         try:
             while RichPresence.rp_running:
-                musica_atual, artista_atual, album_art, album_name = get_music()  # Gets music
+                musica_atual, artista_atual, album_art, album_name, music_duration = get_music()  # Gets music
+                if musica_atual != RichPresence.lastMusic:
+                    RichPresence.agora = int(time.time())
                 if musica_atual and artista_atual:
                     # Atualiza o Rich Presence do Discord
                     RPC.update(
+                        activity_type=ActivityType.LISTENING,
                         details=f"{musica_atual}",
                         state=f"{artista_atual}",
                         large_image=album_art if album_art else "default_image",  # Use a default image if no cover is provided
                         large_text=album_name if album_name else "No album found for this music.",
+                        start=RichPresence.agora if music_duration else None,
+                        end=RichPresence.agora + (int(music_duration)/1000) if music_duration else None,
                     )
                     RichPresence.globalSound = musica_atual
                     RichPresence.globalArtist = artista_atual
+                    RichPresence.lastMusic = musica_atual
                     time.sleep(3)
                 else:
                     RPC.clear()
